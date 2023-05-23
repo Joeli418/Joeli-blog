ea178815f99e5293ea6e079f216101fc3d456735
Joeli418 <zihaoli03@gmail.com>
2023-05-23 15:50:57 +1000

    header change

--- js/script.js
+++ js/script.js
@@ -6,9 +6,33 @@ function closesidenav() {
     document.getElementById("sidenav").style.width = "0";
 }
 
+
+window.onload = function() {
+    document.getElementById("ext1").style.display = "none";
+}
+function toggle_extension1() {
+    if (document.getElementById("ext1").style.display != "none") {
+        document.getElementById("ext1").style.display = "none";
+        document.getElementById("aboutbtn").classList.remove("magentabtn");
+        
+    }
+    else {
+        document.getElementById("ext1").style.display = "flex";
+        document.getElementById("aboutbtn").classList.add("magentabtn");
+        document.getElementById("craftbtn").classList.add("fadebtn");
+        document.getElementById("musicbtn").classList.add("fadebtn");
+        document.getElementById("foodbtn").classList.add("fadebtn");
+        document.getElementById("friendsbtn").classList.add("fadebtn");
+        document.getElementById("tradebtn").classList.add("fadebtn");
+        document.getElementById("aboutbtn2").classList.add("slidebtn")
+    }
+}
+
+
+
 function discordUsername() {
     // Copy the text inside the text field
     navigator.clipboard.writeText("joeli418#9115");
     // Alert the copied text
     alert("Copied discord username: " + "joeli418#9115");
-  }
+}
