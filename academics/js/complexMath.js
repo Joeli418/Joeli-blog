88932cea07e4e987d7eda3954351b34d74ee640a
Joeli418 <zihaoli03@gmail.com>
2024-02-28 00:41:52 +1100

    ELEC1111 stuff

---
+++ academics/js/complexMath.js
@@ -0,0 +1,14 @@
+const cmplx = {
+    r: 0,
+    j: 0
+  }
+
+function execute() {
+    const command = document.getElementById("command").value
+    const answer = document.getElementById("answer")
+    answer.innerHTML = "this is still a work in progress, Sorry!"
+}
+
+function cParallel(a, ja, b, jb) {
+
+}
